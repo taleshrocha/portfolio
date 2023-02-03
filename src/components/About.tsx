f448a89cf22d4f3d00912a313ae31296a2468140
taleshrocha <taleshrochaz@gmail.com>
2023-02-02 23:43:03 -0300

    About base

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,20 @@
 export default function About() {
   return (
-    <div className="flex-col z-10 relative h-full bg-black text-white">
-      <img src="https://avatars.githubusercontent.com/u/61332391?v=4" alt="" />
-      <p>I'm a programer living in Natal, RN. Brazil.</p>
-      <p>
-        I study programing since 2020 in the Federal University of Rio Grande do
-        Norte IMD and DIMAP (which is a second home).
-      </p>
-      <p>I'm interested in Front and Back-end development.</p>
-      <p>Here are somethings that I know:</p>
+    <div className="flex items-center flex-col md:flex-row 
+      z-10 relative h-full bg-gray-900 text-white">
+      <img
+        className="rounded-full w-52 mt-8 border-4 border-sky-700"
+        src="https://avatars.githubusercontent.com/u/61332391?v=4"
+        alt=""
+      />
+      <div className="px-10 py-20 font-semibold text-lg">
+        <p>I'm a programer living in Brazil.</p>
+        <p>
+          I study programing since 2020
+        </p>
+        <p>I'm interested in Front and Back-end development.</p>
+        <p>Here are somethings that I know:</p>
+      </div>
     </div>
   );
 }
