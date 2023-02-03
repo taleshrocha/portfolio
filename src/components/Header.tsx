a7c3f1b9b4fe79e5876f624620fab1012102b953
taleshrocha <taleshrochaz@gmail.com>
2023-02-02 21:49:23 -0300

    Header basics

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { AiFillGithub } from "react-icons/ai";
+
+export default function Header() {
+  return (
+    <div
+      className="flex fixed top-0 bg-black/50 w-full h-10 md:h-14 
+        backdrop-blur-lg items-center justify-between px-16 
+      text-white text-sm md:text-lg"
+    >
+      <p>TALES <span className="font-bold">ROCHA</span></p>
+      <div className="hidden md:inline-flex">
+      <AiFillGithub size={25} />
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
-import Head from 'next/head'
+import Header from "@/components/Header";
+import Head from "next/head";
 
 export default function Home() {
+  //<p>I'm a programer living in Natal, RN. Brazil.</p>
+  //<p>I study programing since 2020 in the Federal University of Rio
+  //  Grande do Norte IMD and DIMAP (which is a second home).</p>
+  //<p>I'm interested in Front and Back-end development.</p>
+  //<p>Here are somethings that I know:</p>
+  //<p>I love to read and I'am learning to play guitar. I also like pizza, but I'm lactose intolerant (didin't ask for that!)</p>
+  //<p>I like to use fancy editors. Like Vim, though I'm still not mature enought to Emacs.</p>
+
+  //<p>Some fun facts</p>
+  //<p>
+  //  I'am reading Game of Thrones
+  //  I have lactose intolerance
+  //</p>
   return (
     <>
       <Head>
@@ -8,9 +22,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className='text-xs text-red-500'>Hello</h1>
+      <main className="flex bg-sky-700 h-full min-h-screen items-center justify-center">
+        <Header/>
+        <p className="whitespace-pre font-bold text-5xl md:text-7xl
+          text-white text-center">
+          {"Hello world!\nMy name is\nTales Rocha"}
+        </p>
       </main>
     </>
-  )
+  );
 }
