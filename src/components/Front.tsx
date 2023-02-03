8c57d2435c4952ce02c886c0e830fa16c8d0451e
taleshrocha <taleshrochaz@gmail.com>
2023-02-03 16:39:02 -0300

    A better typewriter component

--- src/components/Front.tsx
+++ src/components/Front.tsx
@@ -1,21 +1,21 @@
 import Typewriter from "./Typewriter";
 
 export default function Front() {
+  const texts = ["Tales Rocha.", "a programer."]
   return (
-    <div className="sticky top-0 z-0 pt-24 w-full h-screen bg-sky-700">
-      <Typewriter
-        className=" text-white text-2xl blinking-cursor whitespace-pre"
-        baseText={"Hello world\nI'm Tales Rocha"}
+    <div className="flex sticky top-0 z-0 pt-24 w-full h-screen bg-sky-700
+      items-center justify-center">
+      <p
+        className="text-center text-white text-2xl whitespace-pre">
+        {"Hello world!\nI'm "}
+        <Typewriter
+        className="blinking-cursor text-purple-400"
+        baseTexts={texts}
         loop={true}
-        ms={100}
+        typeTime={100}
         sleepTime={1000}
       />
-      <Typewriter
-        className=" text-black text-2xl blinking-cursor whitespace-pre"
-        baseText={"Hello world\nI'm Tales Rocha"}
-        loop={false}
-        ms={200}
-      />
+      </p>
     </div>
   );
 }
--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -1,46 +1,47 @@
 import { useEffect, useState } from "react";
 
 interface TypewriterProps {
-  baseText: string;
+  baseTexts: string[];
   loop: Boolean;
-  ms: number
-  sleepTime?: number
+  typeTime: number;
+  sleepTime?: number;
+  className?: string;
 }
 
-export default function Typewriter({ baseText, loop, ms, sleepTime, ...props }: TypewriterProps) {
+export default function Typewriter({
+  baseTexts,
+  loop,
+  typeTime,
+  sleepTime,
+  ...props
+}: TypewriterProps) {
+  const [index, setIndex] = useState(0);
   const [text, setText] = useState("");
   const [isTyping, setIsTyping] = useState(true);
 
-  function sleep(ms: any) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  function sleep(typeTime: any) {
+    return new Promise((resolve) => setTimeout(resolve, typeTime));
   }
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       if (isTyping) {
-        if (text === baseText && loop) {
-          sleep(sleepTime? sleepTime : 0).then(()=>{
+        if (text === baseTexts[index] && loop) {
+          sleep(sleepTime ? sleepTime : 0).then(() => {
             setIsTyping(false);
-          })
-        }
-        else setText(baseText.slice(0, text.length + 1));
+          });
+        } else setText(baseTexts[index].slice(0, text.length + 1));
       } else {
         if (text === "") {
-          sleep(sleepTime? sleepTime : 0).then(()=>{
+          sleep(sleepTime ? sleepTime : 0).then(() => {
             setIsTyping(true);
-          })
-        }
-        else setText(baseText.slice(0, text.length - 1));
+            if (baseTexts.length > 1) setIndex((index + 1) % baseTexts.length);
+          });
+        } else setText(baseTexts[index].slice(0, text.length - 1));
       }
-    }, ms);
+    }, typeTime);
     return () => clearTimeout(timeout);
   }, [text, isTyping]);
 
-  return (
-    <p 
-      {...props}
-    >
-      {text}
-    </p>
-  );
+  return <span {...props}>{text}</span>;
 }
