3dce018711c7c64a71959aec9ad69a8fb2a65630
taleshrocha <taleshrochaz@gmail.com>
2023-02-06 19:28:30 -0300

    Circle animation

--- src/components/Front.tsx
+++ src/components/Front.tsx
@@ -1,17 +1,24 @@
 import Typewriter from "./Typewriter";
 
 export default function Front() {
+  let circles = [];
+  for (let index = 0; index < 10; index++) {
+    circles.push(
+      <li className=""></li>
+    );
+  }
   return (
     <div
       className="
         flex sticky top-0 z-0 items-center justify-center
-        bg-gradient-to-r from-cyan-500 to-blue-500 
+        bg-gradient-to-t from-cyan-500 to-blue-500 
         w-full h-screen pt-24 
       "
     >
+      <ul className="absolute top-0 left-0 w-full h-full circles">{circles}</ul>
       <p
         className="
-          text-center text-white whitespace-pre font-semibold
+          z-10 text-center text-white whitespace-pre font-semibold
           text-3xl 
           md:text-7xl 
         "
