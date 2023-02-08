39dab5a86e877cb90bb05712785da282bd12b947
taleshrocha <taleshrochaz@gmail.com>
2023-02-08 08:06:12 -0300

    Header transitions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,32 +12,31 @@ export default function Header() {
       flex fixed top-0 z-50 items-center justify-between
       bg-black/50 backdrop-blur-lg text-white
       transition-all duration-300 ease-in-out
-      w-full 
-      ${isOpen && "h-16 flex-col pb-20"}
+      w-screen flex-col
+      ${isOpen && "h-20"}
       h-8 text-sm px-4 
-      md:h-12 md:text-base md:flex-row md:pb-0 md:px-16
+      md:h-12 md:text-base 
       `}
     >
-      <div className="flex justify-between w-full">
-      <p>
-        TALES <span className="font-bold">ROCHA</span>
-      </p>
+      <div className="inline-flex justify-between items-center w-screen md:h-full px-2">
+        <p>
+          TALES <span className="font-bold">ROCHA</span>
+        </p>
 
-      {/*Mobile*/}
-      <div className={`md:hidden inline-flex`}>
         <button
+          className="md:hidden"
           onClick={() => setIsOpen(!isOpen)}
         >
           <CgMenuHotdog size={25} />
         </button>
       </div>
-        
-      </div>
 
-      <div 
+      <div
         className={`
-          ${!isOpen && "hidden"} flex flex-col justify-center items-center 
-          md:flex-row space-x-4 md:space-x-8 space-y-2
+          ${!isOpen && "bottom-24"} absolute flex flex-col justify-center items-center 
+          transition-all duration-1000
+          bottom-0 md:inset-y-0 md:right-4
+          md:flex-row space-x-4 md:space-x-8 space-y-2 md:space-y-0 md:translate-y-0
           `}
       >
         <div className="inline-flex space-x-2 md:space-x-4">
@@ -56,7 +55,7 @@ export default function Header() {
         </div>
         <div className="inline-flex space-x-2 md:space-x-4">
           <a href="https://github.com/taleshrocha" target="_blank">
-            <AiFillGithub 
+            <AiFillGithub
               className="h-[75%] md:h-full"
               size={25} />
           </a>
@@ -64,7 +63,7 @@ export default function Header() {
             href="mailto:taleshrochaz@gmail.com?subject=Let's talk!"
             target="_blank"
           >
-            <MdEmail 
+            <MdEmail
               className="h-[75%] md:h-full"
               size={25} />
           </a>
